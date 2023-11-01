55111445d2023e6fb48e29059730f14546cce71d
Max Rigout <71095568+maxrigout@users.noreply.github.com>
2023-11-01 11:16:49 -0400

    added scripts to install mongo and redis

---
+++ install-scripts/install-mongo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# https://www.mongodb.com/docs/manual/tutorial/install-mongodb-on-ubuntu/
+
+echo "installing mongo db"
+
+sudo apt-get install gnupg curl
+curl -fsSL https://pgp.mongodb.com/server-7.0.asc | \
+   sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
+   --dearmor
+# version 22!!!!!
+# TODO: use this command to check the version: lsb_release -a
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
---
+++ install-scripts/install-redis.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# https://redis.io/docs/install/install-redis/install-redis-on-linux/
+echo "installing redis"
+sudo apt install lsb-release curl gpg
+
+curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
+sudo apt-get update
+sudo apt-get install redis -y
