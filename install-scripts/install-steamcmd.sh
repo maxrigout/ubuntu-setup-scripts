f8c8e2b32b29cb879ddc0c569839fb09a59d5dc6
Max Rigout <71095568+maxrigout@users.noreply.github.com>
2024-01-21 12:08:57 -0500

    added commands for installing steamcmd

--- install-scripts/install-steamcmd.sh
+++ install-scripts/install-steamcmd.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-echo "installing steam cli"
+echo "installing steam cmd"
 # https://developer.valvesoftware.com/wiki/SteamCMD
+# sudo useradd -m steam
+# sudo -u steam -s
+# cd /home/steam
+sudo add-apt-repository multiverse
+sudo dpkg --add-architecture i386
+sudo apt update
+sudo apt install steamcmd
